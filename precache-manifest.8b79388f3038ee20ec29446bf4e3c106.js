df68753062bfad8e7abebbc9d574618c8ee4f21d
Greg Allebach <greg.allebach@gmail.com>
2020-08-22 13:44:25 -0400

    Updates

---
+++ precache-manifest.8b79388f3038ee20ec29446bf4e3c106.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c5d823b3ef7de35bae98e0ff569b192",
+    "url": "/clicky-game-2/index.html"
+  },
+  {
+    "revision": "501479df010cf40e5658",
+    "url": "/clicky-game-2/static/css/main.c81199a8.chunk.css"
+  },
+  {
+    "revision": "7f0dd2c1fabfc49decbd",
+    "url": "/clicky-game-2/static/js/2.b9c6fb10.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/clicky-game-2/static/js/2.b9c6fb10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "501479df010cf40e5658",
+    "url": "/clicky-game-2/static/js/main.c32eff89.chunk.js"
+  },
+  {
+    "revision": "772d894912eeb5ea6f1b",
+    "url": "/clicky-game-2/static/js/runtime-main.d67a457d.js"
+  },
+  {
+    "revision": "afb000838231764b1cd4101a7efbef0c",
+    "url": "/clicky-game-2/static/media/bojack-header.afb00083.jpeg"
+  },
+  {
+    "revision": "9001eb9c9c1519aaa7fcf26514c1dcfd",
+    "url": "/clicky-game-2/static/media/husk.9001eb9c.png"
+  }
+]);
